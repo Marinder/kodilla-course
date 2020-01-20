32076c6981bfd9a0168a48e1701ac050d392358f
Marinder <dkucharski1996@gmail.com>
2020-01-20 19:06:06 +0100

    6.4

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -9,17 +9,21 @@ public class ShapeCollector {
     public void addFigure(Shape shape){
         figure.add(shape);
     }
-    public boolean  removeFigure(Shape shape){
-        return true;
+    public void  removeFigure(Shape shape){
+        figure.remove(shape);
     }
 
     public Shape getFigure(int n){
         return figure.get(n);
 
     }
+
+    public void showFigures(){
+        for(int i = 0; i < figure.size(); i++){
+            System.out.println(figure.get(i));
+        }
+    }
     public int getArrayListSize(){
         return  figure.size();
     }
-
-
 }
