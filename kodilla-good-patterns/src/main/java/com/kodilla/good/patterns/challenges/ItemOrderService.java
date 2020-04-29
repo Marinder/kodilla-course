97abe3bf796b74d5c1b6af543b53bd341bb4fe29
Marinder <dkucharski1996@gmail.com>
2020-04-29 14:04:26 +0200

    challenges 2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ItemOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ItemOrderService.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public class ItemOrderService implements OrderService {
     @Override
     public boolean order(final User user,final LocalDateTime timeOrder,final Item item) {
-        System.out.println("Item: " + item.getName() + " is ordered by " + user.getNick() + " at " + timeOrder.toString());
+        System.out.println("Item: " + item.getName() + " is ordering by " + user.getNick() + " at " + timeOrder.toString());
         return true;
     }
 }
