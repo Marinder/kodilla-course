8de3d2df99961c9f2cea6b6b100ec7ba08e3106c
Marinder <dkucharski1996@gmail.com>
2020-09-01 23:35:51 +0200

    24.2 + 24.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -15,14 +15,13 @@ public class OrderFacade {
     private ShopService shopService;
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
 
-    public void processOrder(final OrderDto order, final Long userId)
+    public void processOrder(final OrderDto order,final Long userId)
             throws OrderProcessingException {
         boolean wasError = false;
         long orderId = shopService.openOrder(userId);
         LOGGER.info("Registering new order, ID: " + orderId);
         if (orderId < 0) {
             LOGGER.error(OrderProcessingException.ERR_NOT_AUTHORISED);
-            wasError = true;
             throw new OrderProcessingException(OrderProcessingException.ERR_NOT_AUTHORISED);
         }
         try {
